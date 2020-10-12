fa7eb2f90963ff4fb45f86685f9fb082ac88bcaf
Val <57106675+vcthuang@users.noreply.github.com>
2020-10-12 11:41:13 -0700

    Client register

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component = {Landing} />
+        <Route exact path="/register" component = {Register} />
+        <Route exact path="/login" component = {Login} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
